dd462fcb1b2817cb0bcac10695e3b183473c7a5a
au645691 <201909157@post.au.dk>
2024-10-02 11:35:13 +0200

    Tested using external logger lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <logger.h>
 
 // put function declarations here:
 void blink();
@@ -11,6 +12,7 @@ void setup() {
   // put your setup code here, to run once:
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  set_log_level(log_levels::INFO);
 }
 
 void loop() {
@@ -21,13 +23,15 @@ void loop() {
 // blink function
 void blink() {
   while (true) {
+    log(log_levels::INFO, "Blinking LED\n", 0);
     // turn the LED on (HIGH is the voltage level)
     digitalWrite(LED_BUILTIN, HIGH);
     // wait for a second
-    delay(1000);
+    delay(500);
     // turn the LED off by making the voltage LOW
     digitalWrite(LED_BUILTIN, LOW);
     // wait for a second
-    delay(1000);
+    delay(10000);
+    log(log_levels::INFO, "LED off\n", 0);
   }
 }
