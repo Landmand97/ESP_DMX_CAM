e57d494cbf9c75ce739cb8a936fdd7c7a92a2573
au645691 <201909157@post.au.dk>
2024-10-02 17:13:18 +0200

    Take picture on reset

---
+++ src/pin_config.h
@@ -0,0 +1,18 @@
+#define SIOD_GPIO_NUM     4
+#define SIOC_GPIO_NUM     5
+#define VSYNC_GPIO_NUM    6
+#define HREF_GPIO_NUM     7
+#define XCLK_GPIO_NUM     15
+#define Y9_GPIO_NUM       16
+#define Y8_GPIO_NUM       17
+#define Y7_GPIO_NUM       18
+#define Y4_GPIO_NUM       8
+#define Y3_GPIO_NUM       9
+#define Y5_GPIO_NUM       10
+#define Y2_GPIO_NUM       11
+#define Y6_GPIO_NUM       12
+#define PCLK_GPIO_NUM     13
+#define LED_GPIO_NUM      48
+
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    -1
